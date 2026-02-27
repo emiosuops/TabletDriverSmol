020495fb98f61b0881deb1f0775bbb2fbaa1d178
emiosuops <emilybopik@gmail.com>
2026-02-27 18:52:43 +0000

    Update Main.c

--- Main.c
+++ Main.c
@@ -3,6 +3,132 @@
 #include <hidsdi.h>
 #include <hidclass.h>
 
+#ifndef ULONG_MAX
+#define	ULONG_MAX	((unsigned long)(~0L))		/* 0xFFFFFFFF */
+#endif
+
+#ifndef LONG_MAX
+#define	LONG_MAX	((long)(ULONG_MAX >> 1))	/* 0x7FFFFFFF */
+#endif
+
+#ifndef LONG_MIN
+#define	LONG_MIN	((long)(~LONG_MAX))		/* 0x80000000 */
+#endif
+
+void* memcpy(void* dest, const void* src, size_t count)
+{
+	/* This would be a prime candidate for reimplementation in assembly */
+	char* in_src = (char*)src;
+	char* in_dest = (char*)dest;
+
+	while (count--)
+		*in_dest++ = *in_src++;
+	return dest;
+}
+int my_isdigit(int c)
+{
+	return ((c >= '0') && (c <= '9'));
+}
+int my_isspace(int c)
+{
+	return ((c == ' ') || (c == '\n') || (c == '\t'));
+}
+int my_isupper(int ch)
+{
+	return (ch >= 'A' && ch <= 'Z');
+}
+int myisalpha(int c)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+	char* letter = alphabet;
+
+	while (*letter != '\0' && *letter != c)
+		++letter;
+
+	if (*letter)
+		return 1;
+
+	return 0;
+}
+long
+strtol(const char* nptr, char** endptr, register int base)
+{
+	register const char* s = nptr;
+	register unsigned long acc;
+	register int c;
+	register unsigned long cutoff;
+	register int neg = 0, any, cutlim;
+
+	/*
+	 * Skip white space and pick up leading +/- sign if any.
+	 * If base is 0, allow 0x for hex and 0 for octal, else
+	 * assume decimal; if base is already 16, allow 0x.
+	 */
+	do {
+		c = *s++;
+	} while (my_isspace(c));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	}
+	else if (c == '+')
+		c = *s++;
+	if ((base == 0 || base == 16) &&
+		c == '0' && (*s == 'x' || *s == 'X')) {
+		c = s[1];
+		s += 2;
+		base = 16;
+	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
+
+	/*
+	 * Compute the cutoff value between legal numbers and illegal
+	 * numbers.  That is the largest legal value, divided by the
+	 * base.  An input number that is greater than this value, if
+	 * followed by a legal input character, is too big.  One that
+	 * is equal to this value may be valid or not; the limit
+	 * between valid and invalid numbers is then based on the last
+	 * digit.  For instance, if the range for longs is
+	 * [-2147483648..2147483647] and the input base is 10,
+	 * cutoff will be set to 214748364 and cutlim to either
+	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
+	 * the number is too big, and we will return a range error.
+	 *
+	 * Set any if any `digits' consumed; make it negative to indicate
+	 * overflow.
+	 */
+	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
+	cutlim = cutoff % (unsigned long)base;
+	cutoff /= (unsigned long)base;
+	for (acc = 0, any = 0;; c = *s++) {
+		if (my_isdigit(c))
+			c -= '0';
+		else if (myisalpha(c))
+			c -= my_isupper(c) ? 'A' - 10 : 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+			any = -1;
+		else {
+			any = 1;
+			acc *= base;
+			acc += c;
+		}
+	}
+	if (any < 0) {
+		acc = neg ? LONG_MIN : LONG_MAX;
+	}
+	else if (neg)
+		acc = -acc;
+	if (endptr != 0)
+		*endptr = (char*)(any ? s - 1 : nptr);
+	return (acc);
+}
 extern NTSYSAPI NTSTATUS NTAPI NtSetTimerResolution(ULONG DesiredResolution, BOOLEAN SetResolution, PULONG CurrentResolution);
 unsigned char buffer[10];
 struct TabletState {
@@ -18,6 +144,13 @@ typedef int BOOL;
 #ifndef _HIDPARSE_H
 #define _HIDPARSE_H
 
+char vid[] = "0X056A";
+char pid[] = "0X00DD";
+char hup[] = "0X000D";
+char hu[] = "0X0001";
+char arx[] = "13440";
+char ary[] = "7560";
+char* end;
 
 
 #pragma warning(error:4100)   // Unreferenced formal parameter
@@ -355,12 +488,18 @@ int OpenDevice(HANDLE* handle, USHORT vendorId, USHORT productId, USHORT usagePa
 
 
 int moon() {
+	char* end;
+	long VID = strtol(vid, &end, 16);
+	long PID = strtol(pid, &end, 16);
+	long HUP = strtol(hup, &end, 16);
+	long HU = strtol(hu, &end, 16);
+	long ARX = strtol(arx, &end, 10);
+	long ARY = strtol(ary, &end, 10);
 
     struct HIDDeviceView tablet = {
         0,
-        0x056A, 0x00DD, 0x000D, 0x0001
+        VID, PID, HUP, HU
     };
-  // Vendor ID , Product ID, UsagePage, Usage
 
     struct HIDDeviceView vmulti = {
         0,
@@ -404,10 +543,9 @@ int moon() {
         UINT16 rawY = (UINT16)(buffer[4] | (buffer[5] << 8));
 
         st.stuff = 0x00010640;
-// 13440 & 7560 = 134.40 x 75.60
-      // wacom pro tablets have 2x counts per mm, would be half area
-        st.x = rawX * 32768 / 13440;
-        st.y = rawY * 32768 / 7560;
+
+        st.x = rawX * 32768 / ARX;
+        st.y = rawY * 32768 / ARY;
         st.x = st.x > 32767 ? 32767 : st.x;
         st.y = st.y > 32767 ? 32767 : st.y;
         DeviceIoControl(
