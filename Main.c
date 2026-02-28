572890d0b64ab4abf1704c5f657514b61036a5b3
emiosuops <emilybopik@gmail.com>
2026-02-28 15:57:39 +0000

    made tablet and area configurable, main loop improve

--- Main.c
+++ Main.c
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include <setupapi.h>
-#include <hidsdi.h>
-#include <hidclass.h>
+#include "settings.h"
+#include "minlib.h"
+#include "hidy.h"
 
 #ifndef ULONG_MAX
 #define	ULONG_MAX	((unsigned long)(~0L))		/* 0xFFFFFFFF */
@@ -15,324 +16,16 @@
 #define	LONG_MIN	((long)(~LONG_MAX))		/* 0x80000000 */
 #endif
 
-void* memcpy(void* dest, const void* src, size_t count)
-{
-	/* This would be a prime candidate for reimplementation in assembly */
-	char* in_src = (char*)src;
-	char* in_dest = (char*)dest;
 
-	while (count--)
-		*in_dest++ = *in_src++;
-	return dest;
-}
-int my_isdigit(int c)
-{
-	return ((c >= '0') && (c <= '9'));
-}
-int my_isspace(int c)
-{
-	return ((c == ' ') || (c == '\n') || (c == '\t'));
-}
-int my_isupper(int ch)
-{
-	return (ch >= 'A' && ch <= 'Z');
-}
-int myisalpha(int c)
-{
-	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		"abcdefghijklmnopqrstuvwxyz";
-	char* letter = alphabet;
-
-	while (*letter != '\0' && *letter != c)
-		++letter;
-
-	if (*letter)
-		return 1;
-
-	return 0;
-}
-long
-strtol(const char* nptr, char** endptr, register int base)
-{
-	register const char* s = nptr;
-	register unsigned long acc;
-	register int c;
-	register unsigned long cutoff;
-	register int neg = 0, any, cutlim;
-
-	/*
-	 * Skip white space and pick up leading +/- sign if any.
-	 * If base is 0, allow 0x for hex and 0 for octal, else
-	 * assume decimal; if base is already 16, allow 0x.
-	 */
-	do {
-		c = *s++;
-	} while (my_isspace(c));
-	if (c == '-') {
-		neg = 1;
-		c = *s++;
-	}
-	else if (c == '+')
-		c = *s++;
-	if ((base == 0 || base == 16) &&
-		c == '0' && (*s == 'x' || *s == 'X')) {
-		c = s[1];
-		s += 2;
-		base = 16;
-	}
-	if (base == 0)
-		base = c == '0' ? 8 : 10;
-
-	/*
-	 * Compute the cutoff value between legal numbers and illegal
-	 * numbers.  That is the largest legal value, divided by the
-	 * base.  An input number that is greater than this value, if
-	 * followed by a legal input character, is too big.  One that
-	 * is equal to this value may be valid or not; the limit
-	 * between valid and invalid numbers is then based on the last
-	 * digit.  For instance, if the range for longs is
-	 * [-2147483648..2147483647] and the input base is 10,
-	 * cutoff will be set to 214748364 and cutlim to either
-	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
-	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
-	 * the number is too big, and we will return a range error.
-	 *
-	 * Set any if any `digits' consumed; make it negative to indicate
-	 * overflow.
-	 */
-	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
-	cutlim = cutoff % (unsigned long)base;
-	cutoff /= (unsigned long)base;
-	for (acc = 0, any = 0;; c = *s++) {
-		if (my_isdigit(c))
-			c -= '0';
-		else if (myisalpha(c))
-			c -= my_isupper(c) ? 'A' - 10 : 'a' - 10;
-		else
-			break;
-		if (c >= base)
-			break;
-		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
-			any = -1;
-		else {
-			any = 1;
-			acc *= base;
-			acc += c;
-		}
-	}
-	if (any < 0) {
-		acc = neg ? LONG_MIN : LONG_MAX;
-	}
-	else if (neg)
-		acc = -acc;
-	if (endptr != 0)
-		*endptr = (char*)(any ? s - 1 : nptr);
-	return (acc);
-}
 extern NTSYSAPI NTSTATUS NTAPI NtSetTimerResolution(ULONG DesiredResolution, BOOLEAN SetResolution, PULONG CurrentResolution);
-unsigned char buffer[10];
-struct TabletState {
-    UINT32 stuff;
-    UINT16 x;
-    UINT16 y;
-} st;
+
 #ifndef BOOL
 typedef int BOOL;
 #endif
 
 
-#ifndef _HIDPARSE_H
-#define _HIDPARSE_H
-
-char vid[] = "0X056A";
-char pid[] = "0X00DD";
-char hup[] = "0X000D";
-char hu[] = "0X0001";
-char arx[] = "13440";
-char ary[] = "7560";
-char* end;
-
-
 #pragma warning(error:4100)   // Unreferenced formal parameter
 
-
-BOOLEAN __stdcall
-HidA_SetFeature(
-	IN    HANDLE   HidDeviceObject,
-	IN    PVOID    ReportBuffer,
-	IN    ULONG    ReportBufferLength
-)
-{
-	ULONG   bytes;
-
-	return DeviceIoControl(HidDeviceObject,
-		IOCTL_HID_SET_FEATURE,
-		ReportBuffer, ReportBufferLength,
-		0, 0,
-		&bytes, NULL) != FALSE;
-}
-
-BOOLEAN __stdcall
-HidA_SetOutputReport(
-	IN    HANDLE   HidDeviceObject,
-	IN    PVOID    ReportBuffer,
-	IN    ULONG    ReportBufferLength
-)
-{
-	ULONG   bytes;
-
-	return DeviceIoControl(HidDeviceObject,
-		IOCTL_HID_SET_OUTPUT_REPORT,
-		ReportBuffer, ReportBufferLength,
-		0, 0,
-		&bytes, NULL) != FALSE;
-}
-
-#define HIDP_PREPARSED_DATA_SIGNATURE1 'PdiH'
-#define HIDP_PREPARSED_DATA_SIGNATURE2 'RDK '
-
-typedef struct _HIDP_SYS_POWER_INFO {
-	ULONG   PowerButtonMask;
-
-} HIDP_SYS_POWER_INFO, *PHIDP_SYS_POWER_INFO;
-
-typedef struct _HIDP_PREPARSED_DATA
-{
-	LONG   Signature1, Signature2;
-	USHORT Usage;
-	USHORT UsagePage;
-
-} HIDP_PREPARSED_DATA;
-
-#endif
-
-typedef struct AHIDP_CAPS
-{
-	USAGE    Usage;
-	USAGE    UsagePage;
-	//    USHORT   InputReportByteLength;
-	//    USHORT   OutputReportByteLength;
-	//    USHORT   FeatureReportByteLength;
-	//    USHORT   Reserved[17];
-
-} BHIDP_CAPS, *CPHIDP_CAPS;
-#ifndef _HIDDLL_H
-#define _HIDDLL_H
-
-
-#define ALLOCATION_SHIFT 4
-#define RANDOM_DATA PtrToUlong(&Hid_Hello)
-
-
-#endif
-int Hid_Hello(char * buff, int len)
-{
-	buff;
-	CHAR ret[] = "Hello\n";
-	ULONG length = (sizeof(ret) < len) ? sizeof(ret) : len;
-
-	return sizeof(ret);
-}
-
-
-BOOLEAN __stdcall
-Hid_GetPreparsedData(
-	IN    HANDLE                  HidDeviceObject,
-	OUT   PHIDP_PREPARSED_DATA  * PreparsedData
-)
-{
-	HID_COLLECTION_INFORMATION info;
-	ULONG                      bytes;
-	PULONG                     buffer;
-	if (!DeviceIoControl(HidDeviceObject,
-		IOCTL_HID_GET_COLLECTION_INFORMATION,
-		0, 0,
-		&info, sizeof(info),
-		&bytes, NULL)) {
-		return FALSE;
-	}
-
-	buffer = LocalAlloc(LMEM_FIXED | LMEM_ZEROINIT, (info.DescriptorSize + (ALLOCATION_SHIFT * sizeof(ULONG))));
-	if (!buffer)
-	{
-		return FALSE;
-	}
-	*buffer = RANDOM_DATA;
-	*PreparsedData = (PHIDP_PREPARSED_DATA)(buffer + ALLOCATION_SHIFT);
-
-	return DeviceIoControl(HidDeviceObject,
-		IOCTL_HID_GET_COLLECTION_DESCRIPTOR,
-		0, 0,
-		*PreparsedData, info.DescriptorSize,
-		&bytes, NULL) != FALSE;
-}
-
-BOOLEAN __stdcall
-Hid_GetAttributes(
-	IN  HANDLE              HidDeviceObject,
-	OUT PHIDD_ATTRIBUTES    Attributes
-)
-{
-	HID_COLLECTION_INFORMATION  info;
-	ULONG                       bytes;
-	if (!DeviceIoControl(HidDeviceObject,
-		IOCTL_HID_GET_COLLECTION_INFORMATION,
-		0, 0,
-		&info, sizeof(info),
-		&bytes, NULL)) {
-		return FALSE;
-	}
-
-	Attributes->Size = sizeof(HIDD_ATTRIBUTES);
-	Attributes->VendorID = info.VendorID;
-	Attributes->ProductID = info.ProductID;
-	Attributes->VersionNumber = info.VersionNumber;
-
-	return TRUE;
-}
-
-BOOLEAN __stdcall
-Hid_FreePreparsedData(
-	IN   PHIDP_PREPARSED_DATA  PreparsedData
-)
-{
-	PULONG buffer;
-
-	buffer = (PULONG)PreparsedData - ALLOCATION_SHIFT;
-
-	if (RANDOM_DATA != *buffer) {
-		return FALSE;
-	}
-
-	LocalFree(buffer);
-	return TRUE;
-}
-
-
-
-#define CHECK_PPD(_x_) \
-   if ((HIDP_PREPARSED_DATA_SIGNATURE1 != (_x_)->Signature1) ||\
-       (HIDP_PREPARSED_DATA_SIGNATURE2 != (_x_)->Signature2)) \
-   { return HIDP_STATUS_INVALID_PREPARSED_DATA; }
-/////////
-NTSTATUS __stdcall
-Hid_GetCaps(
-	IN   PHIDP_PREPARSED_DATA      PreparsedData,
-	OUT  CPHIDP_CAPS                Capabilities
-)
-{
-	ULONG               i;
-	CHECK_PPD(PreparsedData);
-
-	for (i = 0; i < sizeof(HIDP_CAPS) / sizeof(ULONG); i++) {
-		((PULONG)Capabilities)[0] = 0;
-	}
-
-	Capabilities->UsagePage = PreparsedData->UsagePage;
-	Capabilities->Usage = PreparsedData->Usage;
-
-	return HIDP_STATUS_SUCCESS;
-}
 
 ///////////
 // Struct for our device view
@@ -489,12 +182,12 @@ int OpenDevice(HANDLE* handle, USHORT vendorId, USHORT productId, USHORT usagePa
 
 int moon() {
 	char* end;
-	long VID = strtol(vid, &end, 16);
-	long PID = strtol(pid, &end, 16);
-	long HUP = strtol(hup, &end, 16);
-	long HU = strtol(hu, &end, 16);
-	long ARX = strtol(arx, &end, 10);
-	long ARY = strtol(ary, &end, 10);
+	long VID = a_strtol(vid, &end, 16);
+	long PID = a_strtol(pid, &end, 16);
+	long HUP = a_strtol(hup, &end, 16);
+	long HU = a_strtol(hu, &end, 16);
+	long ARX = a_strtol(arx, &end, 10);
+	long ARY = a_strtol(ary, &end, 10);
 
     struct HIDDeviceView tablet = {
         0,
@@ -521,33 +214,34 @@ int moon() {
 
     FreeLibrary(nice);
 
-
 	ULONG CurrentRes = 0;
-
 	NtSetTimerResolution(5024, TRUE, &CurrentRes);
-  
+
+
+    DWORD bytesReturned;
+
+
+    unsigned char buffer[10];
     struct TabletState {
         UINT32 stuff;
         UINT16 x;
         UINT16 y;
-    };
-
-    unsigned char buffer[10];
-    struct TabletState st;
-    DWORD bytesReturned;
-
+    } st;
+    st.stuff = 0x00010640;
     while (1) {
-        ReadFile(tablet.deviceHandle, buffer, 10, NULL, NULL);
+        ReadFile(tablet.deviceHandle, buffer, 10, &bytesReturned, NULL);
+        UINT32 xy = *(const UINT32*)(buffer + 2);
 
-        UINT16 rawX = (UINT16)(buffer[2] | (buffer[3] << 8));
-        UINT16 rawY = (UINT16)(buffer[4] | (buffer[5] << 8));
+        UINT16 rawX = (UINT16)xy;
+        UINT16 rawY = (UINT16)(xy >> 16);
+        
 
-        st.stuff = 0x00010640;
+        int tmp = (rawX << 15) / ARX;
+        st.x = (tmp > 32767) ? 32767 : (UINT16)tmp;
 
-        st.x = rawX * 32768 / ARX;
-        st.y = rawY * 32768 / ARY;
-        st.x = st.x > 32767 ? 32767 : st.x;
-        st.y = st.y > 32767 ? 32767 : st.y;
+        tmp = (rawY << 15) / ARY;
+        st.y = (tmp > 32767) ? 32767 : (UINT16)tmp;
+
         DeviceIoControl(
             vmulti.deviceHandle,
             IOCTL_HID_SET_OUTPUT_REPORT,
@@ -559,7 +253,6 @@ int moon() {
             NULL
         );
     }
-
 
     return 0;
 }
